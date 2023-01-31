2f50daebfff40faa8b87595280685bd9c03bcb88
Will Sams <will@samswebs.com>
2023-01-30 20:56:25 -0500

    Finish implementing basic logger

--- common/logger.cpp
+++ common/logger.cpp
@@ -1,13 +1,47 @@
+#include <chrono>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+
 #include "logger.h"
 
-void Logger::Log(const std::string &message) { log_callback_(message); };
+std::vector<LogEntry> Logger::messages;
 
-void Logger::Err(const std::string &message) { err_callback_(message); };
+std::string CurrentDateTimeToString() {
+  std::time_t now =
+      std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+  std::string output(30, '\0');
+  std::strftime(&output[0], output.size(), "%d-%b-%Y %H:%M:%S",
+                std::localtime(&now));
+  return output;
+};
 
-void Logger::SetLogCallback(std::function<void(const std::string &)> callback) {
+void Logger::SetLogCallback(const LogCallback &callback) {
   log_callback_ = callback;
-};
+}
 
-void Logger::SetErrCallback(std::function<void(const std::string &)> callback) {
+void Logger::SetErrCallback(const LogCallback &callback) {
   err_callback_ = callback;
-};
+}
+
+void Logger::Log(const std::string &message) {
+  LogEntry log_entry;
+  log_entry.type = LOG_INFO;
+  log_entry.message = "LOG: [" + CurrentDateTimeToString() + "]: " + message;
+  messages.push_back(log_entry);
+
+  if (log_callback_) {
+    log_callback_(log_entry);
+  }
+}
+
+void Logger::Err(const std::string &message) {
+  LogEntry log_entry;
+  log_entry.type = LOG_ERROR;
+  log_entry.message = "ERR: [" + CurrentDateTimeToString() + "]: " + message;
+  messages.push_back(log_entry);
+
+  if (err_callback_) {
+    err_callback_(log_entry);
+  }
+}
--- common/logger.h
+++ common/logger.h
@@ -2,16 +2,26 @@
 
 #include <functional>
 #include <string>
+#include <vector>
+
+enum LogType { LOG_INFO, LOG_WARNING, LOG_ERROR };
+
+struct LogEntry {
+  LogType type;
+  std::string message;
+};
 
 class Logger {
 public:
+  using LogCallback = std::function<void(const LogEntry &)>;
+
+  static std::vector<LogEntry> messages;
   void Log(const std::string &message);
   void Err(const std::string &message);
-
-  void SetLogCallback(std::function<void(const std::string &)> callback);
-  void SetErrCallback(std::function<void(const std::string &)> callback);
+  void SetLogCallback(const LogCallback &callback);
+  void SetErrCallback(const LogCallback &callback);
 
 private:
-  std::function<void(const std::string &)> log_callback_;
-  std::function<void(const std::string &)> err_callback_;
+  LogCallback log_callback_;
+  LogCallback err_callback_;
 };

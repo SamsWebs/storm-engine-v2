c9642d9f708f5386cb1dbb574eb03142f9d41ab0
Will Sams <will@samswebs.com>
2023-02-04 08:47:53 -0500

    Begin implementing systems logic

--- specs/ecs.spec.cpp
+++ specs/ecs.spec.cpp
@@ -1,13 +1,45 @@
 #include <igloo/igloo_alt.h>
 
-#include <stormengine2/ecs.h>
+#include "../common/stormengine2/ecs.h"
 
 using namespace igloo;
 
-// Describe(EcsSpec) {
-//     It(should_create_entity) {
-//         ECS ecs;
-//         Entity entity = ecs.CreateEntity();
-//         Assert::That(entity.id, Equals(0));
-//     };
-// };
+Describe(EcsSpec) {
+  It(should_get_identifier_of_entity) {
+    Entity entity = Entity(0);
+    Assert::That(entity.GetId(), Equals(0));
+
+    Entity entity2 = Entity(99);
+    Assert::That(entity2.GetId(), Equals(99));
+  };
+
+  It(should_add_entity_to_system) {
+    System system;
+    Entity entity = Entity(0);
+    system.AddEntityToSystem(entity);
+    Assert::That(system.GetSystemEntities().size(), Equals(1));
+    Assert::That(system.GetSystemEntities()[0].GetId(), Equals(0));
+
+    system.AddEntityToSystem(Entity(88));
+    Assert::That(system.GetSystemEntities().size(), Equals(2));
+    Assert::That(system.GetSystemEntities()[1].GetId(), Equals(88));
+  };
+
+  It(should_remove_entity_from_system) {
+    System system;
+    Entity entity = Entity(0);
+    system.AddEntityToSystem(entity);
+    Assert::That(system.GetSystemEntities().size(), Equals(1));
+    Assert::That(system.GetSystemEntities()[0].GetId(), Equals(0));
+    system.RemoveEntityFromSystem(entity);
+    Assert::That(system.GetSystemEntities().size(), Equals(0));
+  };
+
+  It(should_get_system_entities) {
+    System system;
+    Entity entity = Entity(0);
+    system.AddEntityToSystem(entity);
+    Assert::That(system.GetSystemEntities().size(), Equals(1));
+    Assert::That(system.GetSystemEntities()[0].GetId(), Equals(0));
+  };
+};
--- specs/logger.spec.cpp
+++ specs/logger.spec.cpp
@@ -2,7 +2,7 @@
 
 #include <string>
 
-#include <stormengine2/logger.h>
+#include "../common/stormengine2/logger.h"
 
 using namespace igloo;
 
